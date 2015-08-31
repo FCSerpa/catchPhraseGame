70235676644c8a26b3956cf5f5e949da6c7ab80f
Francis Serpa <fcserpa@gmail.com>
2015-08-31 09:15:28 -0700

    some styling and wrote the README

---
+++ README.md
@@ -0,0 +1,5 @@
+This is a site for a catchphrase style game to test your knowledge of types of dinosaurs.  The user can read a description of a kind of dinosaur and then reveal that animal's name.  Users can also delete dinosaurs from the database and add their own.
+
+The site is made with HTML, CSS, JavaScript, jQuery, Bootstrap and Underscore.  The back end work is done with Express, Node.js, Nodemon, Mongoose, and MongoDB.
+
+In the future, I'd like to add a way to for the computer to test the user's inputs and grade them on their accuracy.
--- seed.js
+++ seed.js
@@ -1,15 +1,16 @@
 var db = require("./models");
 
 var dinoItems = [
-	{name: "Triceratops", text: "Cretaceous herbivore. Quadripedal. Three horns on face."},
+	{name: "Triceratops", text: "Cretaceous herbivore. Quadripedal. Named for the three horns on its face."},
 	{name: "Stegosaurus", text: "Jurassic herbivore.  Quadripedal. Vertical plates down the back, and a spikey tail."},
 	{name: "Archaeopteryx", text: "Jurassic carnivore.  Bipedal.  Tiny, feathered, long considered the first bird."},
 	{name: "Tyrannosaurus", text: "Cretaceous carnivore. Bipedal.  Large predator, known for it's small forelimbs."},
-	{name: "Brontosaurus", text: "Jurassic herbivore. Quadripedal.  Large sauropod.  For a time had to change its name due to a paleontological error, but its original name has since been restored."}
+	{name: "Brontosaurus", text: "Jurassic herbivore. Quadripedal.  Large sauropod.  For a time had to change its name due to a paleontological error, but its original name has since been restored."},
+	{name: "Hummingbird?", text: "Holocene herbivore. Bipedal. Tiny dinosaur, consumes its own weight in nectar every day, fastest animal on earth by size."},
+	{name: "Ankylosaurus," text: "Cretaceous herbivore. Quadripedal. This heavily armored dinosaur has a huge hammer shaped bony growth at the end of its tail."},
 	];
 
-	//name: Hummingbird?
-	//name: Ankylosaurus
+	
 	//name: Deinonychus
 	//name: Coelophysus
 	//name: Dilophosaurus
